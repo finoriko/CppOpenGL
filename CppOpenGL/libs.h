2a5aaec72637f0c179af50fb6710e770f90a8b0f
finoriko <finoriko@Hotmail.com>
2020-04-08 21:07:53 +0900

    링커 라이브러리 연결 및 윈도우창 생성

--- CppOpenGL/libs.h
+++ CppOpenGL/libs.h
@@ -1,2 +1,8 @@
 #pragma once
+
 #include<iostream>
+
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
+#include<glm.hpp>
+#include<SOIL2.h>
--- CppOpenGL/main.cpp
+++ CppOpenGL/main.cpp
@@ -1,6 +1,68 @@
 #include "libs.h"
 
+
+void frambuffer_resize_callback(GLFWwindow* window, int fbw, int fbh)
+{
+	glViewport(0, 0, fbw, fbh);
+}
+
 int main()
 {
+	//Init GLFW
+	glfwInit();
+
+	//Create Window
+	const int WINDOW_WIDTH = 640;
+	const int WINDOW_HEIGHT = 480;
+	int framebufferWidth = 0;
+	int framebufferHeight = 0;
+
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE); //gl_flase 하면 리사이즈 안됨
+
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "CppOpenGL", NULL, NULL);
+
+	glfwSetFramebufferSizeCallback(window, frambuffer_resize_callback);
+
+	//glfwGetFramebufferSize(window, &framebufferWidth, &framebufferHeight);
+	//glViewport(0, 0, framebufferWidth, framebufferHeight);
+
+	glfwMakeContextCurrent(window);
+
+
+	//init glew(needs window and opengl context
+
+	glewExperimental = GL_TRUE;
+
+	//Error
+	if (glewInit() != GLEW_OK)
+	{
+		std::cout << "GLEW FAILED" << std::endl;
+		glfwTerminate();
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		//Update Input
+		glfwPollEvents();
+		//update
+
+		//Draw
+
+		//clear
+		glClearColor(0.f, 0.f, 0.f, 1.f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT | GL_STENCIL_BUFFER_BIT);
+		//draw
+
+		//end draw
+		glfwSwapBuffers(window);
+		glFlush();
+
+	}
+	glfwTerminate();
+
+
 	return 0;
 }
