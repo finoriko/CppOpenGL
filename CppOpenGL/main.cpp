2e66af5aa835c05d295a36b1c0c0f2f780abd197
finoriko <finoriko@Hotmail.com>
2020-04-16 01:08:13 +0900

    MultiTexture add

--- CppOpenGL/main.cpp
+++ CppOpenGL/main.cpp
@@ -251,7 +251,7 @@ int main()
 
 	glBindVertexArray(0);
 
-	//Texture init
+	//Texture 0
 	int image_width = 0;
 	int image_height = 0;
 	unsigned char* image = SOIL_load_image("Images/okami.png", &image_width, &image_height, NULL, SOIL_LOAD_RGBA);
@@ -279,6 +279,35 @@ int main()
 	glActiveTexture(0);
 	glBindTexture(GL_TEXTURE_2D, 0);
 	SOIL_free_image_data(image);
+
+	//Texture 1
+	int image_width1 = 0;
+	int image_height1 = 0;
+	unsigned char* image1 = SOIL_load_image("Images/wood.png", &image_width1, &image_height1, NULL, SOIL_LOAD_RGBA);
+
+
+	GLuint texture1;
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+
+	if (image1)
+	{
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width1, image_height1, 0, GL_RGBA, GL_UNSIGNED_BYTE, image1);
+		glGenerateMipmap(GL_TEXTURE_2D);
+	}
+	else
+	{
+		std::cout << "ERROR::TEXTURE_LOADING_FAILED" << std::endl;
+	}
+
+	glActiveTexture(0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	SOIL_free_image_data(image1);
 	//MAIN LOOP
 	while (!glfwWindowShouldClose(window))
 	{
@@ -297,11 +326,13 @@ int main()
 
 		//update uniforms
 		glUniform1i(glGetUniformLocation(core_program, "texture0"), 0);
-		//glUniform1i(glGetUniformLocation(core_program, "texture1"), 1);
+		glUniform1i(glGetUniformLocation(core_program, "texture1"), 1);
 
 		//Activate texture
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, texture0);
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture1);
 
 		//Bind vertex array object
 		glBindVertexArray(VAO);
