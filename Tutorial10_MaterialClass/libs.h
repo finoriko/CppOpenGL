0ca19229ff988a96a9b6b1c66bd789ad3ecd6ffe
finoriko <finoriko@Hotmail.com>
2020-05-04 19:54:10 +0900

    Material Class Project Setting

---
+++ Tutorial10_MaterialClass/libs.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include<iostream>
+#include<fstream>
+#include<string>
+#include<vector>
+
+#include<GL/glew.h>
+#include<GLFW/glfw3.h>
+
+#include<glm.hpp>
+#include<vec2.hpp>
+#include<vec3.hpp>
+#include<vec4.hpp>
+#include<mat4x4.hpp>
+#include<gtc/matrix_transform.hpp>
+#include<gtc/type_ptr.hpp>
+
+#include<SOIL2.h>
+
+#include"Shader.h"
+#include"Texture.h"
+#include"Material.h"
+struct Vertex
+{
+	glm::vec3 position;
+	glm::vec3 color;
+	glm::vec2 texcoord;
+	glm::vec3 normal;
+};
